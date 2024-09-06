189be8fdf70268777f2be8b0268faa404658a15d
Ben Kirk <benkirk@ucar.edu>
2024-09-05 18:18:56 -0600

    conda env -> pip install develop version, then forced to use libfuse3

---
+++ libexec/wrapper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# following https://hpc.nih.gov/apps/singularity.html
+SCRIPTDIR="$( cd "$( dirname "$(readlink -f ${BASH_SOURCE[0]})" )" >/dev/null 2>&1 && pwd )"
+
+#echo "pwd=$(pwd)"
+#echo "SCRIPTDIR=${SCRIPTDIR}"
+
+cmd="$(basename $0)"
+
+module purge >/dev/null 2>&1 || exit 1
+module load conda >/dev/null 2>&1
+
+make -C ${SCRIPTDIR} --silent ${cmd}-env
+
+conda activate ${SCRIPTDIR}/${cmd}-env/ >/dev/null 2>&1 \
+    || { echo "Cannot 'conda activate ${SCRIPTDIR}/${cmd}-env'"; exit 1; }
+
+# run the wrapped command with any arguments
+${cmd} "$@"
